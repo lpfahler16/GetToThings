6853d2cd71fd1a41d5871bfcb4e205f3350b4a76
LPfahler Macbook Pro <46872232+lpfahler16@users.noreply.github.com>
2020-09-14 22:34:25 -0400

    Adding new data for Core

---
+++ GetToThings/Logic/Core Data/RandomTask.swift
@@ -0,0 +1,15 @@
+//
+//  RandomTask.swift
+//  GetToThings
+//
+//  Created by Logan Pfahler on 9/14/20.
+//  Copyright © 2020 Logan Pfahler. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class RandomTask:RandomThing {
+    
+}
